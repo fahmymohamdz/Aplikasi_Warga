d0bd8ef6dc6f14be70be007272425dc0f3a9587b
FAHMY MOHAMAD <fahmymohamdz@gmail.com>
2026-02-12 11:22:26 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20, fontFamily: "Arial" }}>
+      <h1>Aplikasi Warga</h1>
+      <p>Website sudah berhasil jalan 🎉</p>
+    </main>
+  );
+}
